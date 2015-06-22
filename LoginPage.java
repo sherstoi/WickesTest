4a3f7495c7e58c380287596cdd1eaf21d423de3f
sherstoi <sherstoi83@gmail.com>
2015-06-22 16:31:46 +0300

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -6,44 +6,40 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 /**
- * Applying <Page Object> pattern to Login page.
- * Login page contain elements locator, methods to work with
- * such element and webDriver object (webDriver already initialized).
+ * Applying <Page Object> pattern to Main page.
+ * Main page contain elements locator, method to work with
+ * such element and webDriver object.
  */
-public class LoginPage {
-
-    public static final String URL = MainPage.URL.replace("http", "https") + "login";
-
+public class MainPage {
+    // URL to main page
+    public static final String URL = "http://www.wickes.co.uk/";
+    // reference to web driver
     private WebDriver webDriver;
+    // reference to "login/register" link element
+    @FindBy(css="#header_login_link")
+    private WebElement linkLoginElement;
 
-    @FindBy(css="#j_username")
-    private WebElement emailAddress;
-
-    @FindBy(css="#j_username")
-    private WebElement password;
-
-    @FindBy(css="button[id*='button.login.login']")
-    private WebElement submitButton;
-
-    public LoginPage(WebDriver webDriver) {
+    // constructor
+    public MainPage(WebDriver webDriver) {
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this);
     }
 
-    // open page
-    public void open() {
-        webDriver.get(LoginPage.URL);
-    }
-
+    // get page title
     public String getPageURL() {
         return webDriver.getCurrentUrl();
     }
 
+    // open page
+    public void open() {
+        webDriver.get(MainPage.URL);
+    }
+
     // check whether page opened
     public boolean isPageOpened() {
-        if (!getPageURL().equals(LoginPage.URL)) {
+        if (!getPageURL().equals(MainPage.URL)) {
             System.out.println("Current URL is " + getPageURL());
-            System.out.println("But URL should be " + LoginPage.URL);
+            System.out.println("But URL should be " + MainPage.URL);
 
             return false;
         }
